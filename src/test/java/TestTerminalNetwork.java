a4b55fde22a857c5f78d16711758df51e72c3b4a
Xanatc <alexandrasprodrigues@hotmail.com>
2023-05-19 19:37:35 +0100

    added name of test cases

--- src/test/java/TestTerminalNetwork.java
+++ src/test/java/TestTerminalNetwork.java
@@ -12,12 +12,24 @@ import java.util.stream.IntStream;
 
 @Test
 public class TestTerminalNetwork{
+
+    private static final String NAME_ON = "abc";
+    private static final int NAME_ON_LENGTH = 3;
+    private static final String NAME_OFF_1 = "ab";
+    private static final int NAME_OFF_1_LENGTH = 2;
+    private static final String NAME_OFF_2 = "zxyabcdef";
+    private static final int NAME_OFF_2_LENGTH = 9;
+
+    private static final int MAXCLIENTS_ON = 5000;
+    private static final int MAXCLIENTS_OFF = 5001;
+
+    @Test(testName = "TC1 -- name.length() == " + NAME_ON_LENGTH + " -- ctor")
     public void Test1(){
-        TerminalNetwork t = new TerminalNetwork("abc",50);
+        TerminalNetwork t = new TerminalNetwork(NAME_ON, 50);
 
         int i = 1;
-        while(i <= 30){
-            Client c = new Client("abc"+i, "Normal");
+        while(i <= 30) {
+            Client c = new Client(NAME_ON+i, "Normal");
             Terminal terminal = new Terminal("xyz"+i);
             c.addTerminal(terminal);
 
@@ -26,9 +38,10 @@ public class TestTerminalNetwork{
             i++;
         }
 
-        assertEquals(t.getName(),"abc");
-        assertEquals(t.getMaxClients(),50);
-        assertEquals(t.getClients().size(),30);
+        assertEquals(t.getName(), NAME_ON);
+        assertEquals(t.getMaxClients(), 50);
+        assertEquals(t.getClients().size(), 30);
+        assertEquals(t.getTerminal().size(), 30);
         
         boolean repeated = false;
         int clients_with_same_name = 0;
@@ -61,9 +74,10 @@ public class TestTerminalNetwork{
         }
     }
 
-    @Test(expectedExceptions=IllegalArgumentException.class)
+    @Test(testName = "TC2 -- name.length() == " + NAME_OFF_1_LENGTH + " -- setMaxClients",
+            expectedExceptions = IllegalArgumentException.class)
     public void Test2(){
-        TerminalNetwork t = new TerminalNetwork("ab",239);
+        TerminalNetwork t = new TerminalNetwork(NAME_OFF_1, 239);
 
         t.setMaxClients(240);
         
@@ -78,9 +92,9 @@ public class TestTerminalNetwork{
             i++;
         }
 
-        assertEquals(t.getMaxClients(),240);
-        assertEquals(t.getName(),"ab");
-        assertEquals(t.getClients().size(),180);
+        assertEquals(t.getMaxClients(), 240);
+        assertEquals(t.getName(), NAME_OFF_1);
+        assertEquals(t.getClients().size(), 180);
         
         boolean repeated = false;
         int clients_with_same_name = 0;
@@ -113,10 +127,11 @@ public class TestTerminalNetwork{
         }
     }
 
+    @Test(testName = "TC4 -- name.length() == " + NAME_OFF_2_LENGTH + " -- setname")
     public void Test4(){
-        TerminalNetwork t = new TerminalNetwork("23456981",2099);
-        t.setMaxClients(2100);
-        t.setName("zxyabcdef");
+        TerminalNetwork t = new TerminalNetwork("23456981",2100);
+        //t.setMaxClients(2100);
+        t.setName(NAME_OFF_2);
 
         int i = 1;
         while(i <= 1000){
@@ -158,13 +173,14 @@ public class TestTerminalNetwork{
                 fail("Terminal does not have a client");
             }
         }
-        assertEquals(t.getName(),"zxyabcdef");
-        assertEquals(t.getMaxClients(),2100);
-        assertEquals(t.getClients().size(),1000);
+        assertEquals(t.getName(), NAME_OFF_2);
+        assertEquals(t.getMaxClients(), 2100);
+        assertEquals(t.getClients().size(), 1000);
     }
     
+    @Test(testName = "TC5 -- maxClients == " + MAXCLIENTS_ON + " -- ctor")
     public void Test5(){
-        TerminalNetwork t = new TerminalNetwork("abcd",5000);
+        TerminalNetwork t = new TerminalNetwork("abcd", MAXCLIENTS_ON);
 
         int i = 1;
         while(i <= 3000){
@@ -210,13 +226,14 @@ public class TestTerminalNetwork{
                 fail("Terminal does not have a client");
             }
         }
-        assertEquals(t.getName(),"abcd");
-        assertEquals(t.getMaxClients(),5000);
-        assertEquals(t.getClients().size(),3000);
+        assertEquals(t.getName(), "abcd");
+        assertEquals(t.getMaxClients(), MAXCLIENTS_ON);
+        assertEquals(t.getClients().size(), 3000);
     }
     
+    @Test(testName = "TC6 -- maxClients == " + MAXCLIENTS_OFF + " -- setMaxClients")
     public void Test6(){
-        TerminalNetwork t = new TerminalNetwork("abcde",4999);
+        TerminalNetwork t = new TerminalNetwork("abcde", 4999);
 
         int i = 1;
         while(i <= 3500){
@@ -249,8 +266,8 @@ public class TestTerminalNetwork{
         }
         assertEquals(repeated, false);
 
-        assertThrows(IllegalArgumentException.class, () -> t.setMaxClients(5001));
-        assertEquals(t.getMaxClients(),4999);
+        assertThrows(IllegalArgumentException.class, () -> t.setMaxClients(MAXCLIENTS_OFF));
+        assertEquals(t.getMaxClients(), 4999);
 
         boolean aux = false;
         for (Terminal t1 : t.getTerminal()){
@@ -265,10 +282,11 @@ public class TestTerminalNetwork{
                 fail("Terminal does not have a client");
             }
         }
-        assertEquals(t.getName(),"abcde");
-        assertEquals(t.getClients().size(),3500);
+        assertEquals(t.getName(), "abcde");
+        assertEquals(t.getClients().size(), 3500);
     }
 
+    @Test(testName = "TC9 -- clients.size() == 3001 -- ")
     public void Test8(){
         TerminalNetwork t = new TerminalNetwork("1234567",3000);
         
@@ -322,6 +340,7 @@ public class TestTerminalNetwork{
         }
     }
     
+    @Test(testName = "TC9 -- c.getName() == True -- ")
     public void Test9(){
         TerminalNetwork t = new TerminalNetwork("12345678",4000);
 
@@ -350,7 +369,7 @@ public class TestTerminalNetwork{
                 fail("Terminal does not have a client");
             }
         }
-        assertEquals(t.getMaxClients(),4000);
+        assertEquals(t.getMaxClients(), 4000);
         
         boolean repeated = false;
         int clients_with_same_name = 0;
@@ -368,11 +387,12 @@ public class TestTerminalNetwork{
         }
         assertEquals(repeated, false);
         
-        assertEquals(t.getClients().size(),3099);
+        assertEquals(t.getClients().size(), 3099);
     }
     
+    @Test(testName = "TC10 -- c.getName() == False -- ")
     public void Test10(){
-        TerminalNetwork t = new TerminalNetwork("123456789",4300);
+        TerminalNetwork t = new TerminalNetwork("123456789", 4300);
 
         int i = 1;
         while(i <= 4099){
@@ -407,7 +427,7 @@ public class TestTerminalNetwork{
         }
         assertEquals(repeated, true);
 
-        assertEquals(t.getClients().size(),4100);
+        assertEquals(t.getClients().size(), 4100);
 
         boolean aux = false;
         for (Terminal t1 : t.getTerminal()){
